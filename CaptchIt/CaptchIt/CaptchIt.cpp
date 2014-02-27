2579c34c1e1522f5254a82677bf1af68ae965fa1
Dany Jupille <dany.jupille@he-arc.ch>
2014-02-27 14:40:07 +0100

    Added base skeleton of a Visual C++ application using OpenCV

--- CaptchIt/CaptchIt/CaptchIt.cpp
+++ CaptchIt/CaptchIt/CaptchIt.cpp
@@ -1,11 +1,24 @@
-// CaptchIt.cpp : définit le point d'entrée pour l'application console.
-//
-
 #include "stdafx.h"
 
+using namespace cv;
+using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	Mat img = imread("MyPic.JPG", CV_LOAD_IMAGE_UNCHANGED);
+	
+	if (img.empty()) {
+		cout << "Error: Image cannot be loaded!" << endl;
+		return -1;
+	}
+	
+	namedWindow("MyWindow", CV_WINDOW_AUTOSIZE);
+	imshow("MyWindow", img);
+	
+	waitKey(0);
+	
+	destroyWindow("MyWindow");
+	
 	return 0;
 }
 
--- CaptchIt/CaptchIt/stdafx.h
+++ CaptchIt/CaptchIt/stdafx.h
@@ -10,6 +10,19 @@
 #include <stdio.h>
 #include <tchar.h>
 
+#include <iostream>
 
-
-// TODO: faites référence ici aux en-têtes supplémentaires nécessaires au programme
+#include "opencv2/core/core.hpp"
+#include "opencv2/flann/miniflann.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/photo/photo.hpp"
+#include "opencv2/video/video.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/ml/ml.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/contrib/contrib.hpp"
+#include "opencv2/core/core_c.h"
+#include "opencv2/highgui/highgui_c.h"
+#include "opencv2/imgproc/imgproc_c.h"
